ba490a81d6850db8f9b6f45ab076f91cd3ce4a19
Mithilesh Tiwari <Mithilesh@crowdbotics.com>
2023-02-16 15:36:40 +0530

    Crowdbotics Studio: Update app screens

--- screens/Untitled3/index.js
+++ screens/Untitled3/index.js
@@ -1,20 +1,48 @@
 import React from "react";
-import { StyleSheet, ScrollView, SafeAreaView } from "react-native";
+import { View, StyleSheet, ScrollView, SafeAreaView } from "react-native";
 
-const Untitled3 = () => {
+const Untitled3 = ({
+  route
+}) => {
+  const {} = route.params || {};
   return <SafeAreaView style={styles.safeArea}>
         <ScrollView contentContainerStyle={{
       backgroundColor: '#f0f0f1',
       padding: 10,
       position: 'relative',
       flex: 1
-    }}></ScrollView>
+    }} style={styles.JWjAuWsR}><View style={styles.qtjiOGuV}></View></ScrollView>
     </SafeAreaView>;
 };
 
 const styles = StyleSheet.create({
   safeArea: {
     height: '100%'
+  },
+  JWjAuWsR: {
+    borderWidth: 0,
+    backgroundColor: "#132714",
+    width: 358,
+    height: 574,
+    borderRadius: 0
+  },
+  MtRSrxFn: {
+    height: 60,
+    width: 140,
+    backgroundColor: "#E4E4E4",
+    borderRadius: 69,
+    color: "#777777",
+    position: "absolute",
+    transform: "rotate(12deg)",
+    top: 192,
+    left: 86
+  },
+  qtjiOGuV: {
+    height: 60,
+    width: 140,
+    backgroundColor: "#d91f1f",
+    borderRadius: 0,
+    color: "#777777"
   }
 });
 export default Untitled3;
